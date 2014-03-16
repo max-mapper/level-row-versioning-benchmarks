112643e688c295db0a5ee6e99baf7c4ba741eb9e
Max Ogden <max@maxogden.com>
2014-03-16 16:02:04 -0700

    get reverse test working

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var lexint = require('lexicographic-integer')
 
 var buff = require('fs').readFileSync('package.json')
 
+// creates db with (by default) 1000 ids, each id has 25 versions (25,000 keys total)
 module.exports = function seed(cb) {
   level('test', function(err, db) {
     
     function done() {
+      console.timeEnd('elapsed')
       db.close()
       rimraf('test', function() {})
     }
@@ -15,11 +17,21 @@ module.exports = function seed(cb) {
     var num = process.argv[2] || 1000
     num = +num
     
-    var pending = num * 25
-    for (var i = 0; i < num; i++) {
-      for (var j = 0; j < 25; j++) {
+    var versions = 25
+    var delay = 2000 // in case leveldb is compacting etc
+    var opts = { versions: versions, ids: num }
+    
+    var pending = num * versions
+    
+    for (var i = 1; i < num + 1; i++) {
+      for (var j = 1; j < versions + 1; j++) {
         db.put('ÿ' + lexint.pack(i, 'hex') + 'ÿ' + lexint.pack(j, 'hex'), buff, function() {
-          if (--pending === 0) cb(db, done)
+          if (--pending === 0) {
+            setTimeout(function() {
+              console.time('elapsed')
+              cb(db, opts, done)
+            }, delay)
+          }
         })
       }
     }
--- reverse.js
+++ reverse.js
@@ -1,16 +1,59 @@
 var setup = require('./')
 var lexint = require('lexicographic-integer')
 
-setup(function(db, done) {
-  var opts = {
-    start: '',
-    end: 'ÿ',
+setup(function(db, settings, done) {
+  var startOpts = {
+    start: 'ÿÿ',
+    end: '',
     limit: 1,
     reverse: true
   }
-  db.createReadStream(opts).on('data', function(d) {
-    var parts = d.key.split('ÿ')
-    console.log(lexint.unpack(parts[1], 'hex'), lexint.unpack(parts[2], 'hex'))
-    done()
+  
+  var currentId = settings.ids
+  
+  getOne(startOpts, function(row, raw) {
+    proceed(row, raw)
   })
+  
+  function proceed(row, raw) {
+    if (currentId === 1 && row.id === 1) {
+      console.log('PASSED')
+      done()
+      return
+    }
+    if (row.id !== currentId) {
+      console.error(row.id, currentId)
+      done()
+    } else {
+      currentId--
+      var opts = {
+        end: '',
+        start: 'ÿ' + lexint.pack(row.id, 'hex'),
+        // end: 'ÿ' + raw[1],
+        limit: 1,
+        reverse: true
+      }
+      getOne(opts, function(row, raw) {
+        proceed(row, raw)
+      })
+    }
+  }
+  
+  function getOne(opts, cb) {
+    var rs = db.createReadStream(opts).on('data', function(d) {
+      var parts = d.key.split('ÿ')
+      var doc = {
+        id: lexint.unpack(parts[1], 'hex'),
+        rev: lexint.unpack(parts[2], 'hex')
+      }
+      
+      cb(doc, parts)
+      rs.destroy()
+    })
+    rs.on('error', function(e) {
+      throw e
+    })
+  }
 })
+
+
